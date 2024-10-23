093126b63f24bd8e21d184421f8b30691a0a43ff
angelsn <114720318+angelsn03@users.noreply.github.com>
2024-10-23 16:49:42 -0700

    Reservacion DTO y Service

--- Business/src/main/java/dto/ReservacionDTO.java
+++ Business/src/main/java/dto/ReservacionDTO.java
@@ -4,10 +4,69 @@
  */
 package dto;
 
+import java.time.LocalDateTime;
+
 /**
  *
- * @author martinez
+ * @author angelsn
  */
 public class ReservacionDTO {
+    private Long mesaId;
+    private LocalDateTime fechaHora;
+    private int numeroPersonas;
+    private double costoReservacion;
+
+    public ReservacionDTO(Long mesaId, LocalDateTime fechaHora, int numeroPersonas, double costoReservacion) {
+        this.mesaId = mesaId;
+        this.fechaHora = fechaHora;
+        this.numeroPersonas = numeroPersonas;
+        this.costoReservacion = costoReservacion;
+    }
+
+    public ReservacionDTO(LocalDateTime fechaHora, int numeroPersonas, double costoReservacion) {
+        this.fechaHora = fechaHora;
+        this.numeroPersonas = numeroPersonas;
+        this.costoReservacion = costoReservacion;
+    }
+
+    public ReservacionDTO() {
+    }
+
+    public Long getMesaId() {
+        return mesaId;
+    }
+
+    public void setMesaId(Long mesaId) {
+        this.mesaId = mesaId;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public int getNumeroPersonas() {
+        return numeroPersonas;
+    }
+
+    public void setNumeroPersonas(int numeroPersonas) {
+        this.numeroPersonas = numeroPersonas;
+    }
+
+    public double getCostoReservacion() {
+        return costoReservacion;
+    }
+
+    public void setCostoReservacion(double costoReservacion) {
+        this.costoReservacion = costoReservacion;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservacionDTO{" + "mesaId=" + mesaId + ", fechaHora=" + fechaHora + ", numeroPersonas=" + numeroPersonas + ", costoReservacion=" + costoReservacion + '}';
+    }
     
 }
--- Business/src/main/java/services/ReservacionService.java
+++ Business/src/main/java/services/ReservacionService.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package services;
 
-/**
- *
- * @author martinez
- */
+import dao.ReservacionDAO;
+import dto.ReservacionDTO;
+import models.Cliente;
+import models.Mesa;
+import models.Reservacion;
+import java.time.LocalDateTime;
+
 public class ReservacionService {
+
+    private ReservacionDAO reservacionDAO;
+
+    public ReservacionService() {
+        this.reservacionDAO = new ReservacionDAO();
+    }
+
+    public void crearReservacion(ReservacionDTO reservacionDTO) {
+
+        Cliente cliente = new Cliente();
+        cliente.setId(reservacionDTO.getClienteId());
+
+        Mesa mesa = new Mesa();
+        mesa.setId(reservacionDTO.getMesaId());
+
+        Reservacion reservacion = new Reservacion();
+        reservacion.setCliente(cliente);
+        reservacion.setMesa(mesa);
+        reservacion.setFechaHora(reservacionDTO.getFechaHora());
+        reservacion.setNumeroPersonas(reservacionDTO.getNumeroPersonas());
+        reservacion.setCostoReservacion(reservacionDTO.getCostoReservacion());
+
+        reservacionDAO.crearReservacion(reservacion);
+    }
     
 }
