615aa17763fac1156586416c3e5c7a11db24de39
ang3lfco <ang3lfco@outlook.com>
2024-10-28 22:49:55 -0700

    MesaDAO

--- DataAccess/src/main/java/dao/MesaDAO.java
+++ DataAccess/src/main/java/dao/MesaDAO.java
@@ -3,20 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
-import models.Reservacion;
-import javax.persistence.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import models.Mesa;
 
 /**
  *
  * @author martinez
  */
-public class ReservacionDAO {
+public class MesaDAO {
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.mycompany_DataAccess_jar_1.0-SNAPSHOTPU");
     
-    public void crearReservacion(Reservacion reservacion){
+    public void crearMesa(Mesa mesa){
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-        em.persist(reservacion);
+        em.persist(mesa);
         em.getTransaction().commit();
         em.close();
     }
